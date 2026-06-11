6ab8cb64cf19d8c9631b07443f4ff3e92a6ae6eb
ﾜｲ式会社wingly <wingly.icoration@outlook.com>
2026-06-11 09:50:19 +0900

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,10 @@
-// Firebase Web config for GitHub Pages.
-// Replace null with your Firebase project config object.
-//
-// Example:
-// window.CWW_FIREBASE_CONFIG = {
-//   apiKey: "...",
-//   authDomain: "your-project.firebaseapp.com",
-//   projectId: "your-project",
-//   storageBucket: "your-project.appspot.com",
-//   messagingSenderId: "...",
-//   appId: "..."
-// };
-window.CWW_FIREBASE_CONFIG = null;
+window.CWW_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCT0asCp7V4mGFM_g5LquczpaA8_FRr0Ok",
+  authDomain: "constrained-werewolf.firebaseapp.com",
+  projectId: "constrained-werewolf",
+  storageBucket: "constrained-werewolf.firebasestorage.app",
+  messagingSenderId: "391415171164",
+  appId: "1:391415171164:web:355887e5d41dd5a808be99",
+  measurementId: "G-360RXH0BW8"
+};
 window.CWW_APP_ID = "constrained-word-wolf";
